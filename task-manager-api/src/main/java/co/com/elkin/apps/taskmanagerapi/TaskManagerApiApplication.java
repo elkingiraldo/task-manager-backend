2105487d8971576974ea02ea077a180468d21c90
elkingiraldo <elkin.giraldo.pinedo@gmail.com>
2020-05-09 13:47:55 -0500

    Base of project

---
+++ task-manager-api/src/main/java/co/com/elkin/apps/taskmanagerapi/TaskManagerApiApplication.java
@@ -0,0 +1,13 @@
+package co.com.elkin.apps.taskmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApiApplication.class, args);
+	}
+
+}
---
+++ task-manager-api/src/test/java/co/com/elkin/apps/taskmanagerapi/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.elkin.apps.taskmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
